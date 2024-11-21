5ae7811f8082b824247c48ab2146bd50232ad1f4
Estebamq <quiroz_estebanmarcelo@live.com>
2024-11-21 11:28:59 -0300

    agrego card en index

---
+++ app_cursos/controllers/curseControllers.js
@@ -0,0 +1,40 @@
+
+const getCurse = (req, res)=>{
+
+    const cursos =[
+        {
+            IMG: '../public/images/curso_analisis_datos_ia.jpeg',
+            name: 'Analisis de datos',
+            description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt doloribus dolores facere quam magnam?',
+        },
+        {
+            IMG: '../public/images/curso_diseño_ia.jpeg',
+            name: 'Diseño Grafico',
+            description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt doloribus dolores facere quam magnam?',
+        },
+        {
+            IMG: '../public/images/curso_diseño_web_ia.jpeg',
+            name: 'Diseño web',
+            description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt doloribus dolores facere quam magnam?',
+        },
+        {
+            IMG: '../public/images/curso_machine_l_ia.jpeg',
+            name: 'Machine learning',
+            description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt doloribus dolores facere quam magnam?',
+        },
+        {
+            IMG: '../public/images/curso_programacion_inicial_ia.jpeg',
+            name:'Programacion inicial',
+            description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt doloribus dolores facere quam magnam?',
+        },
+        {
+            IMG: '../public/images/curso_videos_juegos_ia.jpeg',
+            name: 'Programación de videos juegos',
+            description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt doloribus dolores facere quam magnam?',
+        }
+    ]
+
+    res.render('index', {titulo:"FutureAIMentor",cursos});
+}
+
+module.exports = {getCurse};
--- app_cursos/routes/index.js
+++ app_cursos/routes/index.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
+const curseControllers = require('../controllers/curseControllers')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/',curseControllers.getCurse);
 
 module.exports = router;
